1a5b439c0e999cf9d573cc705c1bcb5c984f64cc
Sejan Sadev Wijesinghe <sejanwijesinghe@gmail.com>
2025-12-24 22:15:09 +0530

    Add firebase

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import dayjs from "dayjs";
 import Image from "next/image";
 import {getRandomInterviewCover} from "@/lib/utils";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import DisplayTechIcons from "@/components/DisplayTechIcons";
 
 const InterviewCard = ( {interviewId, userId, role, type, techstack,
                             createdAt}: InterviewCardProps) => {
@@ -23,6 +26,45 @@ const InterviewCard = ( {interviewId, userId, role, type, techstack,
                     <Image src={getRandomInterviewCover()} alt="cover image"
                            width={90} height={90} className="rounded-full object-fit
                     size-[90px]"/>
+
+                    <h3 className="mt-5 capitalize">
+                        {role} Interview
+                    </h3>
+
+                    <div className="flex flex-row  gap-5 mt-3">
+                        <div className="flex flex-row gap-2">
+                            <Image src="/calendar.svg" alt="calendar" width={22} height={22} />
+                            <p>{formattedDate}</p>
+                        </div>
+
+                        <div className="flex flex-row gap-2 items-center">
+                            <Image src="/star.svg" alt="star" width={22} height={22} />
+
+                            <p>{feedback?.totalScore || '---'}/100</p>
+
+                        </div>
+
+                    </div>
+                    <p className="line-clamp-2 mt-5">
+                        {feedback?.finalAssessment || "You haven't taken any interview yet. Take it nowto improve your skills."}
+                    </p>
+                </div>
+                <div className="flex flex-row justify-between">
+                    <DisplayTechIcons techStack={techstack} />
+
+                    <Button className="btn-primary">
+                        <Link
+                            href={
+                                feedback
+                                    ? `/interview/${interviewId}/feedback`
+                                    : `/interview/${interviewId}`
+                            }
+                            className="text-white"
+                        >
+                            {feedback ? "Check Feedback" : "View Interview"}
+                        </Link>
+                    </Button>
+
                 </div>
             </div>
         </div>
